57b26863d2092c373a9def96c3df30303dddf015
GreenColdTea <142627750+GreenColdTea@users.noreply.github.com>
2026-07-29 00:01:49 +0500

    a

--- lang.js
+++ lang.js
@@ -254,7 +254,7 @@ const LANGS = {
       sunky: { title: "Chapga, tepaga, pastga... o'ngga?", desc: "Sunky ni toping va mag'lub eting" },
       fatal: { title: "FATAL Tajriba", desc: "Fatal Error ga o'yiningizga kirishga ruxsat bering" },
       starved: { title: "So'nggi ovqat", desc: "Starved Eggman ni toping va mag'lub eting" },
-      crazy: { title: "SEN SONIC EMASSAN", desc: "Psycho Tails ni mag'lub eting" },
+      crazy: { title: "SEN SONIK EMASSAN", desc: "Psycho Tails ni mag'lub eting" },
       coldsteel: { title: "shaxsiy xusumat yo'q bolakay", desc: "Coldsteel ni toping va mag'lub eting" },
       genesys: { title: "BF O'TDI", desc: "Genesys Eggman ni toping va mag'lub eting" },
       educator: { title: "Mening maktabimga xush kelibsiz!", desc: "Educator ni toping va mag'lub eting" },
